dfd947a3514c58f491a0f645160aad625fc6ae63
amamdik <amamdik@gmail.com>
2024-07-02 22:11:04 +0530

    service registry updated

---
+++ Service_Registry/src/test/java/lk/ijse/gdse66/Service_Registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.Service_Registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
