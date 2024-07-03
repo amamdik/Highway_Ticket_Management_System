bea1d746d812dddfca247d8e9b0228a8ba171066
amamdik <amamdik@gmail.com>
2024-07-03 12:02:34 +0530

    Ticket_Service and Vehicle_Service updated

---
+++ Ticket_Service/src/test/java/lk/ijse/gdse66/Ticket_Service/TicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.Ticket_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Vehicle_Service/src/test/java/lk/ijse/gdse66/Vehicle_Service/VehicleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.Vehicle_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
